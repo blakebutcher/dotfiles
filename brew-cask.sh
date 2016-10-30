c098503047aea0334615dbf7ff17be9a1b16ecf5
Blake Butcher <blake.butcher@gmail.com>
2016-10-30 18:54:33 +1100

    all parameter not required

---
+++ brew-cask.sh
@@ -0,0 +1,45 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# use brew to install packages
+brew install bash-git-prompt
+brew install libsass
+brew install node
+brew install phantomjs
+
+# use brew cask to install apps
+brew cask install 1password
+brew cask install acorn
+brew cask install alfred
+brew cask install atom
+brew cask install charles
+brew cask install dropbox
+brew cask install evernote
+brew cask install firefox
+brew cask install flux
+brew cask install gas-mask
+brew cask install github-desktop
+brew cask install google-chrome
+brew cask install hipchat
+brew cask install interarchy
+brew cask install istat-menus
+brew cask install sourcetree
+brew cask install spectacle
+brew cask install spotify
+brew cask install vivaldi
+
+
+# open Dropbox and sign in
+# wait for ~/Dropbox/Apps/1Password/1Password.agilekeychain to sync then open it
+# open 1Password
+
+
+# disable shortcut in System Preferences > Keyboard > Spotlight > Show Spotlight search
+# open Alfred
+  # open Preferences
+    # open Powerpack tab and enter license from 1Password
+    # open Appearance tab
+      # use Alfred OS X theme
+      # open Options
+        # Hide hat
+        # Hide menu bar icon
